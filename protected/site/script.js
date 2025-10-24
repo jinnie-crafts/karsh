9bf1afd678ab6ea4168ddef83d8ca361a8dcaa89
jinnie-crafts <165927333+jinnie-crafts@users.noreply.github.com>
2025-10-24 09:52:18 +0530

    Add files via upload

---
+++ protected/site/script.js
@@ -0,0 +1,27 @@
+const spyder = document.getElementById("spyder");
+let lines = ['Game Developer','App Developer','Web Developer'];
+let i = 0, j = 0, wait = 0;
+setInterval(() => {
+    if (wait-- > 0) return;
+    if(wait == -1) spyder.innerHTML = "";
+    if (i < lines.length)
+    if(j < lines[i].length)
+    spyder.innerHTML += lines[i][j++];
+    else
+    wait = 1, j = 0, i++;
+    else
+    wait = 0, i = j = 0;
+}, 250);
+
+
+
+
+// // alert message when user leave site 
+// unsaved=true;
+// window.onbeforeunload = function() { 
+//     if (unsaved) 
+//     { 
+//         var _message = "Matt ja bhai...!"; 
+//         return _message; 
+//     } 
+// }
